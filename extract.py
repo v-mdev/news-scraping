c6b68f8063ff29f5fe9c8dd89cccaf7e3a11f793
victo <victormiguel.gf@uma.es>
2025-05-01 20:35:50 +0200

    Correct mistakes and format. Dataframe newspaper-title-date

--- extract.py
+++ extract.py
@@ -1,6 +1,8 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from urllib.request import urlopen, Request
 import time
+from dateutil import parser
+from datetime import date, datetime
 import polars as pl
 
 from bs4 import BeautifulSoup
@@ -11,21 +13,12 @@ def load_url(url: str) -> str:
     r = Request(url, headers = header)
     return urlopen(r).read()
 
-def get_day(news):
-    return news.get_text().split()[0]
 
-def get_updated_hour(news):
-    return news.get_text().split()[-1]
-
-
-#news_number = soup.find_all("h2", {"class": "voc-title"})
-#print(len(news_number))
-
-
-
-#
-#news_title_a_href_all = news_title_a_all['href']
-
+urls = {
+    "https://www.abc.es/": ["time", "datetime", "ABC"],
+    "https://elpais.com/": ["a", "data-date", "El Mundo"],
+    "https://www.elmundo.es/": ["time", "datetime", "El País"]
+}
 
 
 def web_scrapping(url, news_number):
@@ -35,29 +28,28 @@ def web_scrapping(url, news_number):
     html = load_url(url)
     soup = BeautifulSoup(html, "html.parser")
 
-    # news_title_a_all = news_title_all.find("a")
+    date_news = urls[url]
 
-    # news_title_all = soup.find_all("h2", {"class": "voc-title"}, limit = news_number)
+    h2_with_a = soup.select('h2:has(a)', limit=news_number)
+    for h in h2_with_a:
 
-    # news_title_all = soup.find_all("h2", {"class": "voc-title"}, limit = news_number)
-    # news_author_all = soup.find_all("span", {"class": "voc-onplus__author"}, limit = news_number)
-
-    for a in soup.find_all('a', limit=news_number):
+        a = h.find("a")
         link = a['href']
 
         html_href = load_url(link)
         soup_href = BeautifulSoup(html_href, "html.parser")
 
-        news_time = soup_href.find("time")
-        day = get_day(news_time)
-        updated = get_updated_hour(news_time)
+        news_time = soup_href.find(date_news[0], attrs={date_news[1]: True})
+        final_news_time = news_time[date_news[1]]
+        fecha_obj = parser.parse(final_news_time)
+        normal_date = fecha_obj.strftime("%d/%m/%Y %H:%M:%S")
 
         df_news = pl.DataFrame(
             {
+                "newspaper": date_news[2],
                 "title": a.get_text(strip=True),
-                "author": news_author.get_text(strip=True),
-                "time": day,
-                "update_time": updated
+                #"author": news_author.get_text(strip=True),
+                "date": normal_date
             }
         )
 
@@ -71,52 +63,24 @@ def web_scrapping(url, news_number):
 
         time.sleep(10)
 
-
-
-    # for news in news_title_all:
-    #     news_author = news.find_next("span", {"class": "voc-onplus__author"})
-    #     news_title_a = news.find("a")
-    #     news_title_a_href = news_title_a['href']
-
-    #     html_href = load_url(news_title_a_href)
-    #     soup_href = BeautifulSoup(html_href, "html.parser")
-
-    #     news_time = soup_href.find("time")
-
-    #     day = get_day(news_time)
-    #     updated = get_updated_hour(news_time)
-
-    #     df_news = pl.DataFrame(
-    #     {
-    #         "title": news.get_text(strip=True),
-    #         "author": news_author.get_text(strip=True),
-    #         "time": day,
-    #         "update_time": updated
-    #     }
-    #     )
-
-    #     df = pl.concat(
-    #     [
-    #         df,
-    #         df_news,
-    #     ],
-    #     how="vertical",
-    #     )
-
-    #     time.sleep(10)
-
     return df
 
 
-urls = [
-    "https://www.abc.es/",
-    "https://elpais.com/",
-    "https://www.elmundo.es/"
-]
-
 with ThreadPoolExecutor() as executor:
-    future_to_url = [executor.submit(load_url, url) for url in urls]
+    future_to_url = {executor.submit(load_url, url): url for url in list(urls.keys())}
+    df = pl.DataFrame({})
     for future in as_completed(future_to_url):
-        data = future.result()
-        print(data[:50])
-        # TODO: do something with the data
+        url = future_to_url[future]
+        try:
+            df = pl.concat(
+                [
+                    df,
+                    web_scrapping(url, 1),
+                ],
+                how="vertical",
+            )
+          # Llama a la función de scraping con la URL
+            print(df)
+        except Exception as e:
+            print(f"Error al procesar {url}: {e}")
+    print(df)
