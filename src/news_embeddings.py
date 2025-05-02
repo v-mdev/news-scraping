a44d63728549a0e1ee7d63aea17b57ffc9efb174
victo <victormiguel.gf@uma.es>
2025-05-02 13:35:17 +0200

    feat(news_embeddings): add script to transform news title to embeddings

---
+++ src/news_embeddings.py
@@ -0,0 +1,23 @@
+import polars as pl
+from transformers import AutoTokenizer, AutoModel
+import torch
+
+news = pl.read_csv("news.csv")
+
+list_titles = news.get_column("title")
+
+# Load model from HuggingFace Hub
+tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+
+def get_titles_embeddings():
+    # Tokenize sentences
+    encoded_input = tokenizer(list_titles, padding=True, truncation=True, return_tensors='pt')
+
+    # Compute token embeddings
+    with torch.no_grad():
+        model_output = model(input_ids=encoded_input["input_ids"], attention_mask=encoded_input["attention_mask"])
+
+    # Extract CLS token embeddings
+    titles_embeddings = model_output[0][:, 0, :]
+    return titles_embeddings
